9d8da56a12e34f8dd461b442936b12e18d3e6abc
圣风清 <9277@dispd.cn>
2024-03-14 08:37:07 +0800

    test0001
    0001

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,12 @@
 </template>
 
 <style>
-
+*{
+  margin: 0;
+  padding: 0;
+  font-size: 16px;
+  font-family:'Courier New',Courier, monospace;
+}
 </style>
 <script>
 
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -5,10 +5,35 @@ export default {
 </script>
 
 <template>
-  <div>login...
-    <router-link to="/regist">去注册</router-link>
+  <div class="container">
+    <div class="login-container">
+      <h1>123</h1>
+    </div>
   </div>
 </template>
 <style scoped>
 
+.container {
+  //background-color: #f1f1f1;
+  background: url(http://api.0cs.cc) no-repeat;
+  background-size:cover;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.login-container {
+  width: 33%;
+  background-color: rgba(255, 255, 255, 0.68);
+  border-radius: 10px;
+  //margin: 0 auto;
+  height: 100px;
+}
+h1{
+  text-align: center;
+  color: black;
+  line-height: 100px;
+}
 </style>
