183cbcc6a94a0fd55b958806bb6aac2e63dad306
圣风清 <9277@dispd.cn>
2024-03-14 09:25:03 +0800

    test0001
    0001

--- src/App.vue
+++ src/App.vue
@@ -6,11 +6,28 @@
 </template>
 
 <style>
-*{
+* {
   margin: 0;
   padding: 0;
   font-size: 16px;
-  font-family:'Courier New',Courier, monospace;
+  font-family: 'Courier New', Courier, monospace;
+}
+
+:root {
+  --primary-color: #1575cf;
+}
+
+.text-center {
+  text-align: center;
+}
+
+.form-control {
+  margin: 10px 0;
+  font-size: 18px;
+}
+
+.form-control label {
+
 }
 </style>
 <script>
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -7,7 +7,23 @@ export default {
 <template>
   <div class="container">
     <div class="login-container">
-      <h1>123</h1>
+      <h2 class="text-center">用户登录</h2>
+      <form action="#">
+        <div class="form-control">
+          <label for="">用户名</label>
+          <input type="text" name="" id="" placeholder="请输入用户名">
+        </div>
+        <div class="form-control">
+          <label for="">密码</label>
+          <input type="password" name="" id="" placeholder="请输入密码">
+        </div>
+        <div class="form-control">
+          <input type="submit" value="登录">
+          <span>没有账号？
+          <router-link to="/regist"/>去注册
+          </span>
+        </div>
+      </form>
     </div>
   </div>
 </template>
@@ -16,24 +32,19 @@ export default {
 .container {
   //background-color: #f1f1f1;
   background: url(http://api.0cs.cc) no-repeat;
-  background-size:cover;
+  background-size: cover;
   width: 100vw;
   height: 100vh;
   display: flex;
   justify-content: center;
   align-items: center;
+  color: white;
 }
 
 .login-container {
   width: 33%;
-  background-color: rgba(255, 255, 255, 0.68);
+  background-color: rgba(148, 145, 145, 0.68);
   border-radius: 10px;
-  //margin: 0 auto;
-  height: 100px;
-}
-h1{
-  text-align: center;
-  color: black;
-  line-height: 100px;
+  padding: 15px;
 }
 </style>
